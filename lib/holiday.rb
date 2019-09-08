a587187511710493dce9a0ebef77fdef49f4a6dd
Ogein Imoukhuede <o.imoukhuede@aol.com>
2019-09-08 05:39:00 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,11 +1,58 @@
-fruits = ["apple", "orange", "apple"]
+holiday_supplies = {
+  :winter => {
+    :christmas => ["Lights", "Wreath"],
+    :new_years => ["Party Hats"]
+  },
+  :summer => {
+    :fourth_of_july => ["Fireworks", "BBQ"]
+  },
+  :fall => {
+    :thanksgiving => ["Turkey"]
+  },
+  :spring => {
+    :memorial_day => ["BBQ"]
+  }
+}
 
 # Write your code here.
 
-def apple_picker_with_select(fruit_array)
-  fruit_array.select { |item| item == "apple" }
+def second_supply_for_fourth_of_july(supplies_hash)
+  supplies_hash[:summer][:fourth_of_july][1]
 end
 
-def apple_picker_with_collect(fruit_array)
-  fruit_array.collect { |item| item if item == "apple" }.compact
+def add_supply_to_winter_holiday(supplies_hash, supply)
+  supplies_hash[:winter][:new_years] << supply
+end
+
+def add_supply_to_memorial_day(supplies_hash, supply)
+  supplies_hash[:spring][:memorial_day] << supply
+end
+
+def add_new_holiday_with_supplies(supplies_hash, season, new_holiday_name, supplies_list)
+  supplies_hash[season][new_holiday_name] = supplies_list
+
+  supplies_hash
+end
+
+def all_winter_holiday_supplies(supplies_hash)
+  holiday_supplies[:winter].map do |holiday, supplies|
+    supplies
+  end.flatten
+end
+
+def all_supplies_in_holidays(supplies_hash)
+  holiday_supplies.each do |season, holidays|
+    puts "#{season.capitalize}:"
+    holidays.each do |holiday, supplies|
+      puts"  #{holiday.to_s.split('_').map {|w| w.capitalize }.join(' ') }: #{supplies.join(", ")}"
+    end
+  end
+end
+
+def all_holidays_with_bbq(supplies_hash)
+  supplies_hash.map do |season, holidays|
+    holidays.map do |holiday, supplies|
+      holiday if supplies.include?("BBQ")
+    end
+  end.flatten.compact
 end
